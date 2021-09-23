68e63226dc16be93aca9b11ce89f7e797d353995
DishitaShankar24 <90633779+DishitaShankar24@users.noreply.github.com>
2021-09-23 21:19:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,92 +1,55 @@
-var garden,rabbit;
-var gardenImg,rabbitImg;
-var apple,appleImg;
-var greenleaf, greenleafImg;
-var redleaf,redleafImg;
-var orangeleaf,orangeleafImg;
-
+var path,pathway;
+var runner,runneranimation;
+var invisiblewallleft,invisiblewallright;
 function preload(){
-  gardenImg = loadImage("garden.png");
-  rabbitImg = loadImage("rabbit.png");
-  appleImg=loadImage("apple.png");
-  greenleafImg=loadImage("leaf.png")
-  redleafImg=loadImage("redImage.png");
-  orangeleafImg=loadImage("orangeLeaf.png");
+  //pre-load images
+  runneranimation=loadAnimation("Runner-1.png","Runner-2.png")
+  pathway=loadImage("path.png")
+}
+
+function setup(){
+  createCanvas(500,620);
+  //create sprites here
+
+ // create infinite path
+  path=createSprite(250,170,50,300)
+  path.addImage("ground",pathway)
+  path.scale=1.5
+  path.velocityY = 4;
+
+//create jaxon the runner
+runner=createSprite(250,520,50,50)
+  runner.addAnimation("run",runneranimation)
+  runner.scale=0.1
+  
+
+
+//create left invisible wall
+invisiblewallleft=createSprite(10,170,45,900)
+invisiblewallleft.shapeColor="red"
+
+//create right invisible wall
+invisiblewallright=createSprite(510,170,50,900)
+invisiblewallright.shapeColor="red"
+invisiblewallleft.visible=false
+invisiblewallright.visible=false
+
+
 
 }
 
-function setup()
-{
-  
-  createCanvas(550,450);
-  
-// Background Sprite
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
-garden.scale=1.4
-
-//creating green leaf
-greenleaf = createSprite(480,430,30,30);
-greenleaf.scale =0.07;
-greenleaf.addImage(greenleafImg);
-
-//creating red leaf
-redleaf = createSprite(180,400,30,30);
-redleaf.scale =0.065;
-redleaf.addImage(redleafImg);
-
-//creating rabbit
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.12;
-rabbit.addImage(rabbitImg);
-
-}
-
-
-
-function draw()
-{
+function draw() {
   background(0);
-  createapples();
-  createorangeleaf();
-  edges= createEdgeSprites();
-  rabbit.collide(edges);
-  //console.log(rabbit.x)
-   drawSprites();
-  rabbit.x=mouseX
+  if (path.y>620)
+  {
+    path.y=path.height/2
+  }
+  //runner=createSprite(250,520,50,50)
+  runner.x=mouseX;
+
+
+  runner.collide(invisiblewallleft);
+  runner.collide(invisiblewallright);
+drawSprites();
+
 }
-
-//creating apple
-function createapples()
-{
-
-  if(frameCount%80===0)
- {
-   // write your code here 
-   apple = createSprite(50,100,30,30);
-   apple.scale =0.07;
-   apple.addImage(appleImg);
-   apple.velocityY=3;
-   apple.x= Math.round(random(50,450))
-   apple.life=60
- }
-}
-
-
-//creating orange leaf
-function createorangeleaf()
-{
-
-  if(frameCount%70===0)
- {
-   // write your code here 
-   orangeleaf = createSprite(320,20,0,30)
-   orangeleaf.scale =0.07;
-   orangeleaf.addImage(orangeleafImg);
-   orangeleaf.velocityY=2;
-   orangeleaf.x= Math.round(random(50,450))
- }
-}   
-
-rabbit.depth = orangeleaf.depth+1
-rabbit.depth = apple.depth+1;
